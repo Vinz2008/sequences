67180b584ba20e5759bbab8f33098198ebead25f
Vinz2008 <clubfunreading@@gmail.com>
2021-12-07 22:07:00 +0100

    fibonacci in c

--- fibonacci.c
+++ fibonacci.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf('hello');
+    int a = 0;
+    int result = 0;
+    int last_one = 1;
+    int last_two = 1;
+    do {
+        result = last_one + last_two;
+        last_two = last_one;
+        last_one = result;
+        printf(result);
+    } while (a == 0);
     return 0;
 }
