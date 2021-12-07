c6ceae595346fa1d1ca9586932cad53090324328
Vinz2008 <clubfunreading@@gmail.com>
2021-12-07 22:23:09 +0100

    fixed error in fibonnacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 int main() {
-    int a = 0;
-    int result = 0;
-    int last_one = 1;
-    int last_two = 1;
-    do {
+    long result = 0;
+    long last_one = 1;
+    printf("%ld\n",last_one);
+    long last_two = 1;
+    sleep(1);
+    printf("%ld\n",last_two);
+    sleep(1);
+    for (int i=1; i>0; i++) { 
         result = last_one + last_two;
         last_two = last_one;
         last_one = result;
-        printf(result);
-    } while (a == 0);
+        printf("%ld\n",result);
+        sleep(1);
+    }
     return 0;
 }
