f7079cc82849040ef04aef8b34c96d8eb7f5efb9
Vinz2008 <clubfunreading@@gmail.com>
2021-12-09 19:43:34 +0100

    continued assembly

--- fibonacci.asm
+++ fibonacci.asm
@@ -1,13 +1,33 @@
+section.data
+last_one DT 1
+last_two DT 1
+result DT 0
+msg db '$'
+
+
+
 section.start
 	global _start
 _start:
-	mov edx,len
-	mov ecx,msg
+	mov edx,1
+	mov ecx,last_one
 	mov ebx,1
 	mov eax,4
 	int 0x80
 	mov eax,1
+	int 0x80
+	lea di, [msg + 1]
+	mov ax, [résultat]
+	mov bx, 10
+	div bx
+	dec di
+	add dl, '0'
+	mov [di], dl
+	test ax, ax
+	jnz
+	ret
+	mov dl,ax
 
-section.data
-msg db 'Hello World', 0xa
-len equ $ - msg
+
+
+
